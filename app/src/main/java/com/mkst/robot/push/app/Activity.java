9a307932335ff71457fae4ef743385c60b8bbe17
brian <yuni0914@163.com>
2017-07-27 18:24:09 +0800

    Signed-off-by: brian <yuni0914@163.com>

---
+++ app/src/main/java/com/mkst/robot/push/app/Activity.java
@@ -0,0 +1,79 @@
+package com.mkst.robot.push.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * 作者: jiayi.zhang
+ * 时间: 2017/7/27
+ * 描述:初始化Activity
+ */
+
+public abstract class Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在界面未初始化之前调用的初始化窗口
+        initWidows();
+
+        if (initArgs(getIntent().getExtras())) {
+            //得到界面Id并设置到Activity界面中
+            int layId = getContentLayoutId();
+            setContentView(layId);
+            initBefore();
+            initWidget();
+            initData();
+        } else {
+            finish();
+        }
+    }
+
+    /**
+     * 初始化控件调用之前
+     */
+    protected void initBefore() {
+
+    }
+
+    /**
+     * 初始化窗口
+     */
+    protected void initWidows() {
+
+    }
+
+    /**
+     * 初始化相关参数
+     *
+     * @param bundle 参数Bundle
+     * @return 如果参数正确返回True，错误返回False
+     */
+    protected boolean initArgs(Bundle bundle) {
+        return true;
+    }
+
+    /**
+     * 得到当前界面的资源文件Id
+     *
+     * @return 资源文件Id
+     */
+    protected abstract int getContentLayoutId();
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget() {
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
+
+}
---
+++ app/src/main/java/com/mkst/robot/push/app/App.java
@@ -0,0 +1,17 @@
+package com.mkst.robot.push.app;
+
+import android.app.Application;
+
+/**
+ * 作者: jiayi.zhang
+ * 时间: 2017/7/27
+ * 描述:
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
